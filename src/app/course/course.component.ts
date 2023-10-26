4a8d5483d70d9effba5eda37399cca4ab20e11d4
Manuel Gómez <m.gomez49@uniandes.edu.co>
2023-10-26 18:53:01 -0500

    modificarVista_funcionando__BootstrapY_servcNOOOO

---
+++ src/app/course/course.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+import { dataCourses } from './dataCourses';
+import { CourseService } from './course.service';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+  
+  constructor() { }
+  courses: Array<Course> = [];
+  getCourseList(): Array<Course> {
+    return dataCourses;
+  }
+  
+  ngOnInit() {
+    this.courses = this.getCourseList();
+  }
+}
