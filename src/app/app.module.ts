a49d78b809c59c88ea1008723f5c9407fcc41091
Manuel Gómez <m.gomez49@uniandes.edu.co>
2023-10-28 10:45:53 -0500

    tdoo_funcional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CourseModule } from './course/course.module';
-
-import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from "./app.component";
+import { CourseModule } from "./course/course.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CourseModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CourseModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
